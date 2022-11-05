7ea8072fc5639cc762ba0d1a9d110a65cc2a5733
ChenSk12 <1649537125@qq.com>
2022-11-05 13:16:23 +0800

    Initial commit

---
+++ src/main/java/edu/example/express/SpringbootMybatisPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package edu.example.express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPlusGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPlusGeneratorApplication.class, args);
+    }
+
+}
